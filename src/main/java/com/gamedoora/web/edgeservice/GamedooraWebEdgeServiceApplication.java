94307cf39943178d58ebb4023dc91cb256f31a3c
A.P.Rajshekhar <ap.rajshekhar@gmail.com>
2022-07-02 20:22:10 +0530

    Created basic structure and added dependencies

---
+++ src/main/java/com/gamedoora/web/edgeservice/GamedooraWebEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gamedoora.web.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedooraWebEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedooraWebEdgeServiceApplication.class, args);
+	}
+
+}
